718e72e6b5bf255935d31d9e99c52b844cd688aa
SoulSnatcher <93030513+C17H19NO3-SS@users.noreply.github.com>
2025-08-23 10:08:53 +0300

    0.0.1-beta

--- src/Config/Middleware.ts
+++ src/Config/Middleware.ts
@@ -1,11 +1,20 @@
 import type { Handler } from "elysia";
 import { HelloController } from "../Controllers/HelloController";
 import { exampleMiddleware } from "../Middleware/example";
+import { rateLimiter } from "../Middleware/RateLimiter";
 
 export const Middlewares = [
   {
     Class: HelloController,
     middleware: exampleMiddleware as Handler,
     priority: 1,
+  },
+  {
+    Class: HelloController,
+    middleware: rateLimiter({
+      maxRequests: 10,
+      windowMs: 10 * 1000,
+    }),
+    priority: 2,
   },
 ];
